2e7dfb489e363449c8884d5b896ff49ab117623a
mwendell <mwendellsmce@gmail.com>
2026-04-05 13:40:39 -0300

    Adicionando footer, navbar e tailwind no home

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
+import Navbar from "./components/navbar/Navbar"
 import Home from "./pages/home/Home"
+import Footer from "./components/footer/Footer"
 
 function App() {
   return (
     <>
-      <Home />
+      <Navbar />
+      <div className='min-h-[80vh]'>
+        <Home />
+      </div>
+      <Footer />
     </>
   )
 }
---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,23 @@
+function Navbar() {
+  return (
+    <>
+      <div className='w-full flex justify-center py-4 bg-blue-900 text-white'>
+        <div className="container flex justify-between text-lg mx-8">
+          <div className="text-2xl font-bold uppercase">
+            Blog Pessoal
+          </div>
+
+          <div className='flex gap-4'>
+            <div className='hover:underline cursor-pointer'>Postagens</div>
+            <div className='hover:underline cursor-pointer'>Temas</div>
+            <div className='hover:underline cursor-pointer'>Cadastrar tema</div>
+            <div className='hover:underline cursor-pointer'>Perfil</div>
+            <div className='hover:underline cursor-pointer'>Sair</div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Navbar
